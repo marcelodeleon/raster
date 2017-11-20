607e48ed278527aa3c56e356e6e1c55a084613ea
marcelodeleon <mdeleoncosentino@gmail.com>
2017-11-19 23:13:41 -0300

    Add normals to obj render

--- obj.c
+++ obj.c
@@ -39,18 +39,26 @@ void load_data_for_vertex_arrays(Obj *obj)
 {
     int i = 0; // Contador utilizado para cargar los vertices a vertexData.
     int facesCount = list_size(obj->faces);
-    // Asinga la memoria necesaria para guardar todos los vertices.
-    // Hay Faces*3*3 vertices.
+    // Asinga la memoria necesaria para guardar todos los vertices. Hay Faces*3*3 vertices.
     vertexData = (float *)cg_malloc(sizeof(float)*facesCount*3*3);
+    // Asinga la memoria necesaria para guardar todas los normales. Hay Faces*3*3 normales.
+    normalsData = (float *)cg_malloc(sizeof(float)*facesCount*3*3);
+
     printf("Vertex Count: %d\n", facesCount*3*3);
     Block* currentFace = (Block *) obj->faces->head;
     Point3D* v1;
     Point3D* v2;
     Point3D* v3;
+    Point3D* n1;
+    Point3D* n2;
+    Point3D* n3;
     Face* currentFaceData;
     int v1Index;
     int v2Index;
     int v3Index;
+    int n1Index;
+    int n2Index;
+    int n3Index;
     while(currentFace != NULL)
     {
         currentFaceData = (Face *) currentFace->data;
@@ -75,6 +83,27 @@ void load_data_for_vertex_arrays(Obj *obj)
         vertexData[i+6] = v3->x;
         vertexData[i+7] = v3->y;
         vertexData[i+8] = v3->z;
+
+        // Carga normales a la cara normalsData.
+        n1Index = currentFaceData->normals[0];
+        n2Index = currentFaceData->normals[1];
+        n3Index = currentFaceData->normals[2];
+        
+        n1 = (Point3D *)list_get_nth(obj->normals, n1Index);
+        n2 = (Point3D *)list_get_nth(obj->normals, n2Index);
+        n3 = (Point3D *)list_get_nth(obj->normals, n3Index);
+
+        normalsData[i] = n1->x;
+        normalsData[i+1] = n1->y;
+        normalsData[i+2] = n1->z;
+
+        normalsData[i+3] = n2->x;
+        normalsData[i+4] = n2->y;
+        normalsData[i+5] = n2->z;
+
+        normalsData[i+6] = n3->x;
+        normalsData[i+7] = n3->y;
+        normalsData[i+8] = n3->z;
 
         i+=9; // Aumentamos el contador en 9 ya que cada face tiene 3 vertices.
         currentFace = currentFace->next;
@@ -132,8 +161,8 @@ void obj_render(Obj * obj)
     glEnableClientState(GL_VERTEX_ARRAY);
     glVertexPointer(3, GL_FLOAT, 0, vertexData);
 
-    /* glEnableClientState(GL_NORMAL_ARRAY); */
-    /* glNormalPointer(GL_FLOAT,0, normalsData); */
+    glEnableClientState(GL_NORMAL_ARRAY);
+    glNormalPointer(GL_FLOAT,0, normalsData);
 
     /* glEnableClientState(GL_TEXTURE_COORD_ARRAY); */
     /* glTexCoordPointer(2, GL_FLOAT, 0, texData); */
@@ -141,12 +170,16 @@ void obj_render(Obj * obj)
     glDrawArrays(GL_TRIANGLES, 0, vertexCount);
 
     glDisableClientState(GL_VERTEX_ARRAY);
-    /* glDisableClientState(GL_NORMAL_ARRAY); */
+    glDisableClientState(GL_NORMAL_ARRAY);
     /* glDisableClientState(GL_TEXTURE_COORD_ARRAY); */
 }
 
 void obj_free (Obj * obj)
 {
+    // Libero arrays utilizados para dibujar los obj.
+    cg_free(vertexData);
+    cg_free(normalsData);
+
 	/* cg_free(obj->name); */
 	list_free(obj->vertexes);
 	list_free(obj->normals);
